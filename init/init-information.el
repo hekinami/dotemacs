243d50b6d89f88653743cec2bef00d574f07cdae
Zou Bibo <zoubibo@amiunique.net>
2016-06-11 16:58:42 +0800

    irfc mode

--- init/init-information.el
+++ init/init-information.el
@@ -1,4 +1,8 @@
 (require-package 'info+)
 (require 'info+)
 
+(require-package 'irfc)
+(setq irfc-directory (concat (bibo/get-runtimes-dir) (file-name-as-directory "RFC")))
+(setq irfc-assoc-mode t)
+
 (provide 'init-information)
