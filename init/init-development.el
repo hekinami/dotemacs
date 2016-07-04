604a74df13e446c7081bc8df2edd0f02d61aefbf
Zou Bibo <zoubibo@amiunique.net>
2016-07-04 13:29:25 +0800

    enable diff-hl-mode

--- init/init-development.el
+++ init/init-development.el
@@ -4,4 +4,9 @@
 (global-set-key (kbd "C-x g") 'magit-status)
 (global-set-key (kbd "C-x M-g") 'magit-dispatch-popup)
 
+(require-package 'diff-hl)
+
+(add-hook 'magit-post-refresh-hook 'diff-hl-magit-post-refresh)
+(global-diff-hl-mode 1)
+
 (provide 'init-development)
