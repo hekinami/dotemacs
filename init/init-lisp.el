1e368ef46d91154cbc04264c5757c9abf7c14c76
Zou Bibo <zoubibo@amiunique.net>
2016-07-20 15:38:43 +0800

    remove useless stuff

--- init/init-lisp.el
+++ init/init-lisp.el
@@ -5,10 +5,6 @@
                                   (turn-on-eldoc-mode)
                                   (paredit-mode)
                                   (auto-complete-mode)
-                                  (setq skeleton-pair t)
-                                  (mapcar (lambda (token)
-                                            (local-set-key token 'skeleton-pair-insert-maybe))
-                                          '("(" "[" "{" "\""))
                                   (local-set-key (kbd "C-c s") 'elisp-index-search)))
 
 (add-hook 'lisp-interaction-mode-hook (lambda ()
