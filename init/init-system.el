3bd599a33631b56cf30782eecf26caf68ad3e329
Zou Bibo <zoubibo@amiunique.net>
2016-07-22 14:31:35 +0800

    single dir keybinding fix

--- init/init-system.el
+++ init/init-system.el
@@ -6,14 +6,23 @@
 (require 'dired-x)
 
 (require-package 'dired-single)
-(global-set-key [(f5)] 'dired-single-magic-buffer)
-(global-set-key [(control f5)] (function
-                                (lambda nil (interactive)
-                                  (dired-single-magic-buffer default-directory))))
-(global-set-key [(shift f5)] (function
-                              (lambda nil (interactive)
-                                (message "Current directory is: %s" default-directory))))
-(global-set-key [(alt f5)] 'dired-single-toggle-buffer-name)
+
+(defun my-dired-init ()
+  "Bunch of stuff to run for dired, either immediately or when it's
+   loaded."
+  ;; <add other stuff here>
+  (define-key dired-mode-map [return] 'dired-single-buffer)
+  (define-key dired-mode-map [mouse-1] 'dired-single-buffer-mouse)
+  (define-key dired-mode-map "^"
+    (function
+     (lambda nil (interactive) (dired-single-buffer "..")))))
+
+;; if dired's already loaded, then the keymap will be bound
+(if (boundp 'dired-mode-map)
+    ;; we're good to go; just add our bindings
+    (my-dired-init)
+  ;; it's not loaded yet, so add our bindings to the load-hook
+  (add-hook 'dired-load-hook 'my-dired-init))
 
 ;;; ------------------------------------------------------------
 ;;;
