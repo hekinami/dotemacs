d5d94e3bdfa94c32d6726384d9756eb98686e7ef
Zou Bibo <zoubibo@amiunique.net>
2016-07-22 14:31:06 +0800

    org svg export fix

--- init/init-orgnization.el
+++ init/init-orgnization.el
@@ -397,6 +397,30 @@ This would be better done through a customization probably."
 (setq org-export-headline-levels 4)
 (setq org-src-fontify-natively t)
 
+;;; redefine the original one, move the svg related stuff
+(eval-after-load "ox-html"
+  '(progn
+     (defun org-html--format-image (source attributes info)
+       "Return \"img\" tag with given SOURCE and ATTRIBUTES.
+SOURCE is a string specifying the location of the image.
+ATTRIBUTES is a plist, as returned by
+`org-export-read-attribute'.  INFO is a plist used as
+a communication channel."
+       (org-html-close-tag
+        "img"
+        (org-html--make-attribute-string
+         (org-combine-plists
+          (list :src source
+                :alt (if (string-match-p "^ltxpng/" source)
+                         (org-html-encode-plain-text
+                          (org-find-text-property-in-string 'org-latex-src source))
+                       (file-name-nondirectory source)))
+          attributes))
+        info)
+       )
+     )
+  )
+
 ;;; ------------------------------------------------------------
 ;;;
 ;;; org-protocol
