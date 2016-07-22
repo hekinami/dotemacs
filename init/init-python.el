71c3c185028c6e5f69d4fd511ed533e725e9d557
Zou Bibo <zoubibo@amiunique.net>
2016-07-22 14:32:03 +0800

    django support

--- init/init-python.el
+++ init/init-python.el
@@ -1,17 +1,25 @@
 (require-package 'jedi)
 
-(add-hook 'python-mode-hook 'yas-minor-mode)
-(add-hook 'python-mode-hook 'jedi:setup)
-(add-hook 'python-mode-hook (lambda ()
-                              (setq ac-sources (append ac-sources '(ac-source-yasnippet)))))
-
+(setq python-environment-directory (concat (bibo/get-runtimes-dir) ".python-environments"))
+(setq jedi:environment-root "python3dev")
+(setq jedi:environment-virtualenv '("virtualenv" "--system-site-packages" "--always-copy" "--quiet"))
 (setq jedi:setup-keys t)
 (setq jedi:complete-on-dot t)
 (setq jedi:tooltip-method nil)
 (setq python-indent-guess-indent-offset nil)
 
+(add-hook 'python-mode-hook (lambda ()
+                              (jedi:setup)
+                              (yas-minor-mode)
+                              (setq ac-sources (append ac-sources '(ac-source-yasnippet)))))
+
+
 (require-package 'traad)
 (setq traad-server-program "traad")
 (setq traad-auto-revert t)
+
+(require-package 'python-django)
+(require 'python-django)
+(global-set-key (kbd "C-x j") 'python-django-open-project)
 
 (provide 'init-python)
