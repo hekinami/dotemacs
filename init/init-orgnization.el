2fc11684545682b343a691820e82e39a29d874e2
Zou Bibo <zoubibo@amiunique.net>
2017-12-26 02:47:49 +0800

    add task with current checkbox item text

--- init/init-orgnization.el
+++ init/init-orgnization.el
@@ -686,6 +686,18 @@ a communication channel."
 ;;; some customization
 ;;;
 ;;; ------------------------------------------------------------
+;;; Capture a new TODO entry when a checkbox item checked.
+;;; Two properties defined for this purpose:
+;;; 1. CHECKED-THEN-TODO, if t, then add new TODO entry after checkbox checked
+;;; 2. TODO-TEMPLATE, the value if the key for template, if nil, then capture template
+;;;    could be selected from list.
+(defvar bibo/current-item-content ""
+  "Used to save content of current item of list")
+
+(defun bibo/get-current-item-content nil
+  "Return current content of current item of list, used in capture template"
+  bibo/current-item-content)
+
 (defun bibo/org-at-item-checked-checkbox-p ()
   "Check if current org checkbox is checked"
   (org-list-at-regexp-after-bullet-p "\\(\\[[X]\\]\\)[ \t]+"))
@@ -694,8 +706,16 @@ a communication channel."
   "Set CHECKED-THEN-TODO property to `t' to open new TODO entry after checked"
   (apply orig-fun args)
   (when (and (string= "t" (org-entry-get nil "CHECKED-THEN-TODO")) 
-             (bibo/org-at-item-checked-checkbox-p)) 
-    (org-capture nil "t")))
+             (bibo/org-at-item-checked-checkbox-p))
+    (save-excursion
+      (back-to-indentation)
+      (let* ((pl (car (cdr (org-element-context))))
+             (cbegin (plist-get pl :contents-begin))
+             (cend (plist-get pl :contents-end)))
+        (setq bibo/current-item-content
+              (s-trim (buffer-substring-no-properties cbegin cend)))
+        ))
+    (org-capture nil (org-entry-get nil "TODO-TEMPLATE"))))
 
 (advice-add 'org-ctrl-c-ctrl-c :around #'bibo/org-checkbox-checked-to-todo-advice)
 
