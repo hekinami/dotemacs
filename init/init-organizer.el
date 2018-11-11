e2a260220b6b5fdd3e8ec902d39c0eb3d9a68639
Zou Bibo <zoubibo@amiunique.net>
2018-11-11 17:04:31 +0800

    relocate org capture template

--- init/init-organizer.el
+++ init/init-organizer.el
@@ -107,11 +107,7 @@
 ;;; capture
 ;;;
 ;;; ------------------------------------------------------------
-  (let ((template-file (expand-file-name ".org-capture-templates" user-emacs-directory)))
-    (when (file-exists-p template-file)
-      (load-file template-file)
-      )
-    )
+  (load (locate-stuff-file "org-capture-templates") t)
 
 ;;; ------------------------------------------------------------
 ;;;
