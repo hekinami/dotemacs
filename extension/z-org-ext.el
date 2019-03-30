b638c8c695402423d3dc877db1d5b2f9d850a32e
Zou Bibo <zoubibo@amiunique.net>
2019-03-30 16:10:54 +0800

    only consider screenshot capture for org for linux

--- extension/z-org-ext.el
+++ extension/z-org-ext.el
@@ -5,40 +5,21 @@
 (defun z/org-screenshot ()
   "Take a screenshot into a time stamped unique-named file in the same directory as the org-buffer and insert a link to this file."
   (interactive)
-  (when *is-windows*
-    (let* ((capturer "\"C:\\Program Files (x86)\\IrfanView\\i_view32.exe\" /clippaste /convert ")
-           (buffer-file-name-no-ext (file-name-sans-extension (file-name-nondirectory (buffer-file-name))))
-           (buffer-file-dir (file-name-directory (buffer-file-name)))
-           (buffer-file-dir-img (concat buffer-file-dir "images/"))
-           (exist-dir-img (file-accessible-directory-p buffer-file-dir-img))
-           (target-dir (if (not exist-dir-img)
-                           buffer-file-dir
-                         buffer-file-dir-img))
-           (target-file
-            (replace-regexp-in-string "/" "\\"
-                                      (concat (concat target-dir (uuidgen-4)) ".png") t t)))
-      (call-process-shell-command (concat capturer target-file) nil nil nil)
-      (insert (concat "[[file:./" (if exist-dir-img "images/" "") (file-name-nondirectory target-file)  "]]")))
-    )
-  (when *is-linux*
-    (let* ((capturer (concat "python " user-emacs-directory "tools/capclip.py "))
-           p	     (buffer-file-name-no-ext (file-name-sans-extension (file-name-nondirectory (buffer-file-name))))
-           (buffer-file-dir (file-name-directory (buffer-file-name)))
-           (buffer-file-dir-img (concat buffer-file-dir "images/"))
-           (exist-dir-img (file-accessible-directory-p buffer-file-dir-img))
-           (target-dir (if (not exist-dir-img)
-                           buffer-file-dir
-                         buffer-file-dir-img))
-           (target-file
-            (replace-regexp-in-string "/" "/"
-                                      (concat (concat target-dir (uuidgen-4)) ".png") t t)))
-      (if (= 0 (call-process-shell-command (concat capturer target-file) nil))
-          (insert (concat "[[file:./" (if exist-dir-img "images/" "") (file-name-nondirectory target-file)  "]]"))
-        (message "no images in clipboard")))
-    )
+  (let* ((capturer (concat "python " user-emacs-directory "tools/capclip.py "))
+         p	     (buffer-file-name-no-ext (file-name-sans-extension (file-name-nondirectory (buffer-file-name))))
+         (buffer-file-dir (file-name-directory (buffer-file-name)))
+         (buffer-file-dir-img (concat buffer-file-dir "images/"))
+         (exist-dir-img (file-accessible-directory-p buffer-file-dir-img))
+         (target-dir (if (not exist-dir-img)
+                         buffer-file-dir
+                       buffer-file-dir-img))
+         (target-file
+          (replace-regexp-in-string "/" "/"
+                                    (concat (concat target-dir (uuidgen-4)) ".png") t t)))
+    (if (= 0 (call-process-shell-command (concat capturer target-file) nil))
+        (insert (concat "[[file:./" (if exist-dir-img "images/" "") (file-name-nondirectory target-file)  "]]"))
+      (message "no images in clipboard")))
   )
-
-
 
 (defun z/org-delete-linked-file-in-point ()
   "delete a file in point if exists."
@@ -53,5 +34,6 @@
       )
     )
   )
+
 
 (provide 'z-org-ext)
