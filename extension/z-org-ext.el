d5f530eb754f35b424e5ba7ee797cdfcf08f2580
Zou Bibo <zoubibo@amiunique.net>
2020-10-05 20:23:21 +0800

    polished screenshot insert/delete stuff for org-mode

--- extension/z-org-ext.el
+++ extension/z-org-ext.el
@@ -7,7 +7,7 @@
 (defun z/org-screenshot ()
   "Take a screenshot into a time stamped unique-named file in the same directory as the org-buffer and insert a link to this file."
   (interactive)
-  (let* ((capturer (concat "python " user-emacs-directory "tools/capclip.py "))
+  (let* ((capturer (concat "/usr/bin/python3 " user-emacs-directory "tools/capclip.py "))
          p	     (buffer-file-name-no-ext (file-name-sans-extension (file-name-nondirectory (buffer-file-name))))
          (buffer-file-dir (file-name-directory (buffer-file-name)))
          (buffer-file-dir-img (concat buffer-file-dir "images/"))
@@ -15,26 +15,34 @@
          (target-dir (if (not exist-dir-img)
                          buffer-file-dir
                        buffer-file-dir-img))
+         (uuid (uuidgen-4))
          (target-file
           (replace-regexp-in-string "/" "/"
-                                    (concat (concat target-dir (uuidgen-4)) ".png") t t)))
+                                    (concat (concat target-dir uuid) ".png") t t)))
     (if (= 0 (call-process-shell-command (concat capturer target-file) nil))
         (insert (concat "[[file:./" (if exist-dir-img "images/" "") (file-name-nondirectory target-file)  "]]"))
       (message "no images in clipboard")))
   )
 
 (defun z/org-delete-linked-file-in-point ()
-  "delete a file in point if exists."
+  "Delete a file in point if exists."
   (interactive)
-  (let* ((raw-string (or (thing-at-point 'filename) "neverexists"))
-	 (rel-filename (replace-regexp-in-string "^file:" "" raw-string))
-	 (full-name (expand-file-name rel-filename)))
-    (if (file-exists-p full-name)
-	(if (y-or-n-p (format "delete %s " full-name))
-	    (delete-file full-name))
-      (message "no file can be deleted")
-      )
-    )
+  (let* ((context (org-element-context))
+         (element-type (car context))
+         (element-props (car (cdr context))))
+    (when (and (eq element-type 'link)
+               (string= "file" (plist-get element-props :type)))
+      (let ((filename (plist-get element-props :path)))
+        (if (file-exists-p filename)
+	    (if (y-or-n-p (format "Delete %s ? " filename))
+                (progn
+                  (delete-file filename)
+                  (message (format "File %s deleted" filename))
+                  (let ((beg (org-element-property :begin context))
+                        (end (org-element-property :end context)))
+                    (delete-region beg end))))
+          (message "file not existing")
+          ))))
   )
 
 (defvar z/org-clock-in-todo-keywords '("DOING" "READING")
